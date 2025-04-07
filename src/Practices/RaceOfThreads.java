b30ad18427a071f3b89c492b5ed71b45fd3dd484
Muharrem Aslan <muharrem5352@gmail.com>
2025-04-07 21:53:27 +0300

    RaceOfThreads An ArrayList consisting of numbers from 1 to 100 is to be created. Then, this ArrayList is to be divided into four equal parts, each containing 25 elements. Four separate threads are to be designed to find odd and even numbers within each of these 25-element ArrayLists.

---
+++ src/Practices/RaceOfThreads.java
@@ -0,0 +1,65 @@
+package Practices;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class RaceOfThreads implements Runnable {
+    public List<Integer> evens = new ArrayList<>();
+    public List<Integer> odds = new ArrayList<>();
+    public List<Integer> numbers;
+
+    public RaceOfThreads() {
+        this.numbers = IntStream.rangeClosed(1, 100).boxed().toList();
+    }
+
+    @Override
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+
+        int start = 0, end = 0;
+        if (threadName.equals("Thread-0")) {
+            start = 1; end = 25;
+        } else if (threadName.equals("Thread-1")) {
+            start = 26; end = 50;
+        } else if (threadName.equals("Thread-2")) {
+            start = 51; end = 75;
+        } else if (threadName.equals("Thread-3")) {
+            start = 76; end = 100;
+        }
+
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                evens.add(i);
+            } else {
+                odds.add(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        RaceOfThreads race = new RaceOfThreads();
+
+        Thread t1 = new Thread(race);
+        Thread t2 = new Thread(race);
+        Thread t3 = new Thread(race);
+        Thread t4 = new Thread(race);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Çift Sayılar: " + race.evens);
+        System.out.println("Tek Sayılar: " + race.odds);
+    }
+}
